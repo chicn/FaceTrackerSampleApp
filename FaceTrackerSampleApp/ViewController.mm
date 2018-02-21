008cc7caa985300982872280c9bb80f10e69c73f
Chihiro Shimamura <chihiros92@suou.waseda.jp>
2018-02-21 13:44:33 +0900

    Tweak

--- FaceTrackerSampleApp/ViewController.mm
+++ FaceTrackerSampleApp/ViewController.mm
@@ -19,6 +19,8 @@
 #import <AVFoundation/AVFoundation.h>
 #import <CoreMedia/CoreMedia.h>
 
+#include <iostream>
+
 @interface ViewController ()
 <AVCaptureVideoDataOutputSampleBufferDelegate>
 
@@ -61,7 +63,7 @@
 
     faceQueue = dispatch_queue_create("com.f.faceQueue", DISPATCH_QUEUE_SERIAL);
 
-    AVCaptureDevice *captureDevice;  // デバイスを取得
+    AVCaptureDevice *captureDevice;
     for (AVCaptureDevice *device in [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo]) {
         if (device.position == AVCaptureDevicePositionFront) {
             captureDevice = device;
@@ -72,20 +74,20 @@
         [NSException raise:@"" format:@"AVCaptureDevicePositionBack not found"];
     }
 
-    session = [[AVCaptureSession alloc] init];  // セッションを作成
+    session = [[AVCaptureSession alloc] init];
 
-    [session beginConfiguration];  // セッションの設定
+    [session beginConfiguration];
     session.sessionPreset = AVCaptureSessionPreset352x288;
 
-    AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:&error];  // インプットを作成して追加
+    AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:&error];
     if (error) {
         [NSException raise:@"" format:@"AVCaptureDeviceInput not found"];
     }
     [session addInput:deviceInput];
 
-    AVCaptureVideoDataOutput *videoOutput = [[AVCaptureVideoDataOutput alloc] init];  // アウトプットを作成して追加
-    videoOutput.videoSettings = @{(id)kCVPixelBufferPixelFormatTypeKey : @(kCVPixelFormatType_32BGRA) }; // 設定
-    [videoOutput setSampleBufferDelegate:self queue:dispatch_get_main_queue()]; // 出力先
+    AVCaptureVideoDataOutput *videoOutput = [[AVCaptureVideoDataOutput alloc] init];
+    videoOutput.videoSettings = @{(id)kCVPixelBufferPixelFormatTypeKey : @(kCVPixelFormatType_32BGRA) };
+    [videoOutput setSampleBufferDelegate:self queue:dispatch_get_main_queue()];
     [session addOutput:videoOutput];
 
     [session commitConfiguration];
@@ -118,121 +120,13 @@
     unsigned char *baseBuffer = (unsigned char *)CVPixelBufferGetBaseAddress(pixelBuffer);
 
     // Prepare image for mtcnn
-    cv::Mat img = cv::Mat(height, width, CV_8UC4, baseBuffer, bytesPerRow); //put buffer in open cv, no memory copied
+    cv::Mat img = cv::Mat(height, width, CV_8UC4, baseBuffer, bytesPerRow);
     if(!img.data){
-        cout<<"Reading video failed"<<endl;
+        std::cout << "Reading video failed" << std::endl;
         return;
     }
 
     CVPixelBufferUnlockBaseAddress(pixelBuffer, kCVPixelBufferLock_ReadOnly);
-
-    cv::cvtColor(img, img, CV_BGRA2BGR);
-    if(!img.data) cout<<"Reading video failed!"<<endl;
-
-
-    // Prepare mtcnn face detection
-    vector<cv::Rect> rects;
-    vector<float> confidences;
-    std::vector<std::vector<cv::Point>> alignment;
-
-    NSDate *startDate = [NSDate new];
-
-    std::vector<MtcnnResult> faceDetectionResults = faceDetector.run(img, FaceDetector::WOWO);
-
-    NSTimeInterval interval = [[NSDate date] timeIntervalSinceDate:startDate];
-    NSLog(@"inerval: %fms", interval * 1000 );
-
-    // Initialize measurement points
-    static std::vector<cv::Point2f> kalman_points(68, cv::Point2f(0.0, 0.0));
-
-    // Initialize prediction points
-    static std::vector<cv::Point2f> predict_points(68, cv::Point2f(0.0, 0.0));
-
-    // Kalman Filter Setup (68 Points Test)
-    static const int stateNum = 68 * 4;
-    static const int measureNum = 68 * 2;
-
-    KalmanFilter KF(stateNum, measureNum, 0);
-    Mat state(stateNum, 1, CV_32FC1);
-    Mat processNoise(stateNum, 1, CV_32F);
-    Mat measurement = Mat::zeros(measureNum, 1, CV_32F);
-    cv::Mat prevgray, grayImage;
-    static std::vector<cv::Point2f> prevTrackPts(68, cv::Point2f(0, 0));
-    static std::vector<cv::Point2f> nextTrackPts;
-    cv::cvtColor(img, grayImage, CV_BGR2GRAY);
-    int flag = -1;
-    for (const auto& result : faceDetectionResults) {
-        cv::Rect rect = result.bb;
-        dlib::rectangle r(rect.tl().x, rect.tl().y, rect.br().x, rect.br().y);
-        dlib::full_object_detection shape = landmarkDetector(dlib::cv_image<uint8_t>(grayImage), r);
-
-        if (flag == -1) {
-            cvtColor(img, prevgray, CV_BGR2GRAY);
-            const dlib::full_object_detection& d = shape;
-            for (int i = 0; i < d.num_parts(); i++) {
-                prevTrackPts[i].x = d.part(i).x();
-                prevTrackPts[i].y = d.part(i).y();
-            }
-            flag = 1;
-        }
-        // Update Kalman Filter Points
-        const dlib::full_object_detection& d = shape;
-        for (int i = 0; i < d.num_parts(); i++) {
-            kalman_points[i].x = d.part(i).x();
-            kalman_points[i].y = d.part(i).y();
-        }
-
-        // Kalman Prediction
-        Mat prediction = KF.predict();
-
-        for (int i = 0; i < 68; i++) {
-            predict_points[i].x = prediction.at<float>(i * 2);
-            predict_points[i].y = prediction.at<float>(i * 2 + 1);
-        }
-
-        static bool redetected = true;
-        if (prevgray.data) {
-            std::vector<uchar> status;
-            std::vector<float> err;
-            calcOpticalFlowPyrLK(prevgray, prevgray, prevTrackPts, nextTrackPts, status, err);
-            std::cout << "variance:" <<calDistanceDiff(prevTrackPts, nextTrackPts) << std::endl;
-            // if the face is moving so fast, use dlib to detect the face
-            double diff = calDistanceDiff(prevTrackPts, nextTrackPts);
-            if (diff > 1.0) {
-                const dlib::full_object_detection& d = shape;
-                std::cout<< "DLIB" << std::endl;
-                for (int i = 0; i < d.num_parts(); i++) {
-                    cv::circle(img, cv::Point2f(d.part(i).x(), d.part(i).y()), 1, cv::Scalar(0, 0, 255), -1);
-                    nextTrackPts[i].x = d.part(i).x();
-                    nextTrackPts[i].y = d.part(i).y();
-                }
-            } else if (diff <= 1.0 && diff > 0.005){
-                // In this case, use Optical Flow
-                std::cout<< "Optical Flow" << std::endl;
-                for (int i = 0; i < nextTrackPts.size(); i++) {
-                    cv::circle(img, nextTrackPts[i], 1, cv::Scalar(255, 0, 0), -1);
-                }
-            } else {
-                // In this case, use Kalman Filter
-                std::cout<< "Kalman Filter" << std::endl;
-                for (int i = 0; i < predict_points.size(); i++) {
-                    cv::circle(img, predict_points[i], 1, cv::Scalar(0, 255, 0), -1);
-                    nextTrackPts[i].x = predict_points[i].x;
-                    nextTrackPts[i].y = predict_points[i].y;
-                }
-                redetected = false;
-            }
-        } else {
-            redetected = true;
-        }
-        std::swap(prevTrackPts, nextTrackPts);
-        std::swap(prevgray, grayImage);
-
-        // draw?
-        cv::rectangle(img, rect, cv::Scalar(255, 127, 255), 1);
-        for (int i = 0; i < shape.num_parts(); ++i)
-            cv::circle(img, cv::Point((int)shape.part(i).x(), (int)shape.part(i).y()), 1, Scalar(127,255,191));
-    }
 
     // BGRイメージをRGBの配列に戻してpixel_bufferに戻す
     long location = 0;
